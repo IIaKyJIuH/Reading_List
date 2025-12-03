6f79f7013e3e343aadb4300a445b4ca22d98f070
Pakulin Sergei <pakylin3@mail.ru>
2025-12-03 21:51:49 +0300

    upd config.py

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "localhost"
+    DATABASE_URL: str = "sqlite:///app.db"
 
     model_config = SettingsConfigDict(env_file=".env")
 
